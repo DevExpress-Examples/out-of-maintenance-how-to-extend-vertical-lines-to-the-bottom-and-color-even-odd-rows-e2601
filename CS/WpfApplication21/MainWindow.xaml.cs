0c5761b2b29912d0460b70e0dbf3ab65386db39c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication21/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for (int i = 0; i < 10; i++)
+                list.Add(new TestData() { Text = "Row" + i, Number = i });
+            grid.DataSource = list;
+        }
+    }
+
+    public class TestData
+    {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to extend vertical lines to the bottom and color even/odd rows
+
+
+<p>To achieve this goal, override the DataPresenterTemplate so it provides appearance both for even and odd rows.<br /><br /><strong>Update: for now, you can use the TableView.AlternateRowBackground and TableView.AlternationCount properties to specify even and odd row colors.</strong></p>
+
+<br/>
+
+
