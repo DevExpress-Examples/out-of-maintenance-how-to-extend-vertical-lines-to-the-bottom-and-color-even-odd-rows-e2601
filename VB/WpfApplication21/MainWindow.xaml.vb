d4e9c0146d0583ae3ce8122fb0b23c192b4454e4
DevExpressExampleBot <fakeEmail>
2022-12-17 03:25:18 +0400

    Source auto update [skip ci]

--- VB/WpfApplication21/MainWindow.xaml.vb
+++ VB/WpfApplication21/MainWindow.xaml.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Linq
@@ -16,90 +15,46 @@ Imports DevExpress.Xpf.Grid
 Imports DevExpress.Data
 
 Namespace WpfApplication21
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
 
-			Dim list As New List(Of TestData)()
-			For i As Integer = 0 To 9
-				list.Add(New TestData() With {.Text = "Row" & i, .Number1 = i, .Number2 = i, .Number3 = i, .Number4 = i, .Number5 = i})
-			Next i
-			grid.ItemsSource = list
-			grid.Columns(0).Fixed = DevExpress.Xpf.Grid.FixedStyle.Left
-			grid.Columns(2).Fixed = DevExpress.Xpf.Grid.FixedStyle.Right
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits System.Windows.Window
 
-			AddHandler Loaded, AddressOf MainWindow_Loaded
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As System.Collections.Generic.List(Of WpfApplication21.TestData) = New System.Collections.Generic.List(Of WpfApplication21.TestData)()
+            For i As Integer = 0 To 10 - 1
+                list.Add(New WpfApplication21.TestData() With {.Text = "Row" & i, .Number1 = i, .Number2 = i, .Number3 = i, .Number4 = i, .Number5 = i})
+            Next
 
-		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim column As GridColumn = grid.Columns(1)
+            Me.grid.ItemsSource = list
+            Me.grid.Columns(CInt((0))).Fixed = DevExpress.Xpf.Grid.FixedStyle.Left
+            Me.grid.Columns(CInt((2))).Fixed = DevExpress.Xpf.Grid.FixedStyle.Right
+            AddHandler Me.Loaded, New System.Windows.RoutedEventHandler(AddressOf Me.MainWindow_Loaded)
+        End Sub
 
-			grid.TotalSummary.Add(New GridSummaryItem() With {.FieldName = column.FieldName, .SummaryType = SummaryItemType.Sum, .ShowInColumn = column.FieldName})
+        Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim column As DevExpress.Xpf.Grid.GridColumn = Me.grid.Columns(1)
+            Me.grid.TotalSummary.Add(New DevExpress.Xpf.Grid.GridSummaryItem() With {.FieldName = column.FieldName, .SummaryType = DevExpress.Data.SummaryItemType.Sum, .ShowInColumn = column.FieldName})
+            column = Me.grid.Columns(3)
+            Me.grid.TotalSummary.Add(New DevExpress.Xpf.Grid.GridSummaryItem() With {.FieldName = column.FieldName, .SummaryType = DevExpress.Data.SummaryItemType.Sum, .ShowInColumn = column.FieldName})
+        End Sub
+    End Class
 
-			column = grid.Columns(3)
+    Public Class TestData
 
-			grid.TotalSummary.Add(New GridSummaryItem() With {.FieldName = column.FieldName, .SummaryType = SummaryItemType.Sum, .ShowInColumn = column.FieldName})
-		End Sub
-	End Class
+        Public Property Text As String
 
-	Public Class TestData
-		Private privateText As String
-		Public Property Text() As String
-			Get
-				Return privateText
-			End Get
-			Set(ByVal value As String)
-				privateText = value
-			End Set
-		End Property
-		Private privateNumber1 As Integer
-		Public Property Number1() As Integer
-			Get
-				Return privateNumber1
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber1 = value
-			End Set
-		End Property
-		Private privateNumber2 As Integer
-		Public Property Number2() As Integer
-			Get
-				Return privateNumber2
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber2 = value
-			End Set
-		End Property
-		Private privateNumber3 As Integer
-		Public Property Number3() As Integer
-			Get
-				Return privateNumber3
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber3 = value
-			End Set
-		End Property
-		Private privateNumber4 As Integer
-		Public Property Number4() As Integer
-			Get
-				Return privateNumber4
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber4 = value
-			End Set
-		End Property
-		Private privateNumber5 As Integer
-		Public Property Number5() As Integer
-			Get
-				Return privateNumber5
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber5 = value
-			End Set
-		End Property
-	End Class
+        Public Property Number1 As Integer
+
+        Public Property Number2 As Integer
+
+        Public Property Number3 As Integer
+
+        Public Property Number4 As Integer
+
+        Public Property Number5 As Integer
+    End Class
 End Namespace
